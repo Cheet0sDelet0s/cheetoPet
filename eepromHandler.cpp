83e56ee84cfae58c09770eca2f0b13aab6dba771
cheeto <molesfordaysyt@gmail.com>
2025-08-26 09:14:10 +0100

    Reapply "item placing system overhaul/refactor, now uses vector arrays, way more modular. new save method supporting vector arrays"
    This reverts commit 063ed46f4e698d4804b3e3085a5a949ec7c7e57e.

--- eepromHandler.cpp
+++ eepromHandler.cpp
@@ -2,36 +2,81 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#define EEPROM_ADDRESS 0x57 // Check your chip
+#define EEPROM_ADDRESS 0x57 
 #define EEPROM_SIZE 4096    // AT24C32 = 4KB
 
-void writeStructEEPROM(uint16_t addr, SaveGame& savegame) {
-  const byte* ptr = (const byte*)(const void*)&savegame;
-  for (size_t i = 0; i < sizeof(SaveGame); i++) {
-    Wire.beginTransmission(EEPROM_ADDRESS);
-    Wire.write((addr + i) >> 8);    // MSB
-    Wire.write((addr + i) & 0xFF);  // LSB
-    Wire.write(ptr[i]);
-    Wire.endTransmission();
-    delay(5); // Required write delay
-  }
+void eepromWriteByte(uint16_t addr, uint8_t data) {
+  Wire.beginTransmission(EEPROM_ADDRESS);
+  Wire.write((addr >> 8) & 0xFF); // MSB
+  Wire.write(addr & 0xFF);        // LSB
+  Wire.write(data);
+  Wire.endTransmission();
+  delay(5); // EEPROM write cycle
 }
 
-SaveGame readStructEEPROM(uint16_t addr) {
-  SaveGame savegame;
-  byte* ptr = (byte*)(void*)&savegame;
-  for (size_t i = 0; i < sizeof(SaveGame); i++) {
-    Wire.beginTransmission(EEPROM_ADDRESS);
-    Wire.write((addr + i) >> 8);    // MSB
-    Wire.write((addr + i) & 0xFF);  // LSB
-    Wire.endTransmission();
+uint8_t eepromReadByte(uint16_t addr) {
+  Wire.beginTransmission(EEPROM_ADDRESS);
+  Wire.write((addr >> 8) & 0xFF);
+  Wire.write(addr & 0xFF);
+  Wire.endTransmission();
 
-    Wire.requestFrom(EEPROM_ADDRESS, 1);
-    if (Wire.available()) {
-      ptr[i] = Wire.read();
-    } else {
-      ptr[i] = 0xFF; // Default/fail value
-    }
+  Wire.requestFrom(EEPROM_ADDRESS, 1);
+  if (Wire.available()) {
+    return Wire.read();
   }
-  return savegame;
+  return 0xFF; // Default on fail
 }
+
+uint16_t saveVectorToEEPROM(uint16_t addr, const std::vector<ItemList> &vec) {
+  eepromWriteByte(addr++, vec.size()); // Save size
+  for (const auto &item : vec) {
+    eepromWriteByte(addr++, item.type);
+    eepromWriteByte(addr++, item.x);
+    eepromWriteByte(addr++, item.y);
+  }
+  return addr;
+}
+
+uint16_t loadVectorFromEEPROM(uint16_t addr, std::vector<ItemList> &vec) {
+  vec.clear();
+  uint8_t size = eepromReadByte(addr++);
+  for (int i = 0; i < size; i++) {
+    ItemList item;
+    item.type = eepromReadByte(addr++);
+    item.x = eepromReadByte(addr++);
+    item.y = eepromReadByte(addr++);
+    vec.push_back(item);
+  }
+  return addr;
+}
+
+// void writeStructEEPROM(uint16_t addr, SaveGame& savegame) {  // legacy save game
+//   const byte* ptr = (const byte*)(const void*)&savegame;
+//   for (size_t i = 0; i < sizeof(SaveGame); i++) {
+//     Wire.beginTransmission(EEPROM_ADDRESS);
+//     Wire.write((addr + i) >> 8);    // MSB
+//     Wire.write((addr + i) & 0xFF);  // LSB
+//     Wire.write(ptr[i]);
+//     Wire.endTransmission();
+//     delay(5); // Required write delay
+//   }
+// }
+
+// SaveGame readStructEEPROM(uint16_t addr) {
+//   SaveGame savegame;
+//   byte* ptr = (byte*)(void*)&savegame;
+//   for (size_t i = 0; i < sizeof(SaveGame); i++) {
+//     Wire.beginTransmission(EEPROM_ADDRESS);
+//     Wire.write((addr + i) >> 8);    // MSB
+//     Wire.write((addr + i) & 0xFF);  // LSB
+//     Wire.endTransmission();
+
+//     Wire.requestFrom(EEPROM_ADDRESS, 1);
+//     if (Wire.available()) {
+//       ptr[i] = Wire.read();
+//     } else {
+//       ptr[i] = 0xFF; // Default/fail value
+//     }
+//   }
+//   return savegame;
+// }
